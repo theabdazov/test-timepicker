b395df8ed4f22998895b19e39531058dc17baae2
Nursultan <abdazovnursultan@gmail.com>
2020-09-29 17:29:22 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BsLocaleService} from 'ngx-bootstrap/datepicker';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { ruLocale } from 'ngx-bootstrap/locale';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'teste-timepicker';
+export class AppComponent implements OnInit{
+  bootstrap1: Date = new Date();
+  bootstrap2 = '12:45';
+
+  constructor(private localeService: BsLocaleService) {
+    defineLocale('ru', ruLocale);
+  }
+
+  ngOnInit(): void {
+    this.localeService.use('ru');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { TimepickerModule } from 'ngx-bootstrap/timepicker';
+import {FormsModule} from '@angular/forms';
+import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AccordionModule} from 'ngx-bootstrap/accordion';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MAT_DATE_LOCALE, MatFormFieldModule, MatInputModule} from '@angular/material';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TimepickerModule.forRoot(),
+    FormsModule,
+    NgxMaterialTimepickerModule,
+    BrowserAnimationsModule,
+    AccordionModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMomentDateModule
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'ru-RU'},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
