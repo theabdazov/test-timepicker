235f45bc5e773c01e3bd039e5024f3b391c8132c
Nursultan <abdazovnursultan@gmail.com>
2020-09-29 18:22:58 +0600

    fix

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+// Install express server
 const express = require('express');
-
+const path = require('path');
 const app = express();
-
-app.use(express.static('./dist/teste-timepicker'));
-
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/teste-timepicker/'}),
-);
-
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/teste-timepicker'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/teste-timepicker/index.html'));
+});
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
